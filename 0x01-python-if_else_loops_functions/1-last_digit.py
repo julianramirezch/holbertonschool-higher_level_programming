b0f0b28bb967709660289b1a97427279eb6c701f
julianramirezch <julianramirezch1@gmail.com>
2020-04-28 12:36:36 -0500

    [change]: condition order

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,7 +10,7 @@ else:
 
 if last > 5:
     print("{}{} is {} and is greater than 5".format(strn, number, last))
-elif last < 6 and last != 0:
-    print("{}{} is {} and is less than 6 and not 0".format(strn, number, last))
-else:
+elif last == 0:
     print("{}{} is {} and is 0".format(strn, number, last))
+else:
+    print("{}{} is {} and is less than 6 and not 0".format(strn, number, last))
