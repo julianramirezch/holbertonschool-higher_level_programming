09c321a8f670e0213aa9ccff99714d89b170b949
julianramirezch <julianramirezch1@gmail.com>
2020-04-28 12:22:47 -0500

    [fix]: is greater than 5

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ else:
     last = number % 10
 
 if last > 5:
-    print('Last digit of', number, 'is', last, 'and is greater tan 5')
+    print('Last digit of', number, 'is', last, 'and is greater than 5')
 elif last < 6 and last != 0:
     print('Last digit of', number, 'is', last, 'and is less than 6 and not 0')
 else:
