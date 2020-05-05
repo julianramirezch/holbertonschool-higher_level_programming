581be41c88fa7bcc9a3679e450ec99fceee4172e
julianramirezch <julianramirezch1@gmail.com>
2020-05-05 12:30:22 -0500

    [fix[ string format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    rows = len(matrix)
-    for x in matrix:
-        col = 0
-        if rows == 1 and enumerate(matrix) == 0:
-            print('')
-        else:
+    if matrix == [[]]:
+        print('')
+    else:
+        for x in matrix:
+            col = 0
             for y in x:
                 if col == (len(x) - 1):
-                    print('{}'.format(y), end='')
+                    print('{:d}'.format(y), end='')
                 else:
-                    print('{}'.format(y), end=' ')
+                    print('{:d}'.format(y), end=' ')
                 col += 1
             print('')
