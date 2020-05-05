8d531b0e4892233031a4997ffff5d8805a4a2983
julianramirezch <julianramirezch1@gmail.com>
2020-05-05 12:14:07 -0500

    [fix]: len (my_list) -1

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     list_copy = my_list[:]
     if idx < 0:
         return list_copy
-    elif idx > len(my_list):
+    elif idx > (len(my_list) - 1):
         return list_copy
     else:
         list_copy[idx] = element
