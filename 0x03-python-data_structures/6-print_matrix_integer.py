cac986d9bb54dc7bcd61c4d284e5b7923f711938
julianramirezch <julianramirezch1@gmail.com>
2020-05-05 12:15:39 -0500

    [fix]: matrix:

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == [[]]:
-        print()
-    else:
+    if matrix:
         for x in matrix:
             col = 0
             for y in x:
