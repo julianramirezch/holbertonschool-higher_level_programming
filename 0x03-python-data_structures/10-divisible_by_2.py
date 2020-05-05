b50be60657d7ce3611ad3c67ad1daf9a8a44d9f6
julianramirezch <julianramirezch1@gmail.com>
2020-05-05 15:14:05 -0500

    [refactor]

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list == []:
-        return
-    else:
-        new_list = my_list[:]
-        for i in my_list:
-            if i % 2 == 0:
-                new_list[i] = True
-            else:
-                new_list[i] = False
-        return new_list
+    new_list = []
+    for i in my_list:
+        if i % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return new_list
