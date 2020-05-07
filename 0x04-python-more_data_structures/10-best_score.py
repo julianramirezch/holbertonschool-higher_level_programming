7c9f96870c89fda45e60bfa0791fbe3b23eef8bc
julianramirezch <julianramirezch1@gmail.com>
2020-05-07 16:06:49 -0500

    [fix] none condition

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        return max(a_dictionary)
-    else:
+    if a_dictionary is None or a_dictionary == {}:
         return
+    else:
+        return max(a_dictionary)
