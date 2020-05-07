30f1888c5c901293d6a457a97da0f765a862fbc6
julianramirezch <julianramirezch1@gmail.com>
2020-05-07 13:08:34 -0500

    [refactor using set]

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-
-
-def add_list(copy_list):
-    res = 0
-    for i in copy_list:
-        res += int(i)
-    return res
-
-
 def uniq_add(my_list=[]):
-    new_list = []
-    copy_list = sorted(my_list)
-    for i in range(len(copy_list)):
-        if copy_list[i] != copy_list[i - 1]:
-            new_list.append(copy_list[i])
+    res = 0
+    new_list = set(my_list)
 
-    return add_list(new_list)
+    for i in new_list:
+        res += i
+
+    return res
