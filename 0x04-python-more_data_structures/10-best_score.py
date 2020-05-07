3d0134f99cc15edf02418fdb78ca2301af4deef7
julianramirezch <julianramirezch1@gmail.com>
2020-05-07 16:15:03 -0500

    [fix]: resturn key value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        maximus = max(a_dictionary)
-        return maximus
-    else:
-        return
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
