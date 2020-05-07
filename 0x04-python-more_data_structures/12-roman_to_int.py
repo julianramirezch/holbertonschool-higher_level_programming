1cea23ab3dde2b61c3bd6fc5de470739876e25a2
julianramirezch <julianramirezch1@gmail.com>
2020-05-07 17:06:23 -0500

    [add]: add first prototype to checker

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    for i in roman_string:
+        if i in dic:
+            num += dic.get(i)
+    return num
