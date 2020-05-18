c66cbeca5a2214f8f9eccc5cba49eaa9b4636cf0
julianramirezch <julianramirezch1@gmail.com>
2020-05-18 11:06:30 -0500

    [add]: README.md and function that prints x elements of a list.

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    cont = 0
+    try:
+        for i in range(x):
+            print('{:d}'.format(my_list[i]), end='')
+            cont += 1
+        print('')
+    except IndexError:
+        print('')
+    finally:
+        return cont
