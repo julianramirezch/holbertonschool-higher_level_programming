2a98162da18944932c69f551a36ae67c8025daa2
julianramirezch <julianramirezch1@gmail.com>
2020-05-18 12:03:53 -0500

    [fix]: delete condition

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if type(value) is int:
-            print('{:d}'.format(value))
-            return True
+        print('{:d}'.format(value))
+        return True
     except ValueError:
         return False
+    return False
