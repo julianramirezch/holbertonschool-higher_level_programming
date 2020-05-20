7b8e4b4eaffffa9a30c13fc87999cf4620d9db57
julianramirezch <julianramirezch1@gmail.com>
2020-05-19 20:29:38 -0500

    [fix]: Documentation

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,7 +17,7 @@ class Square:
     """
 
     def __init__(self, size=0, position=(0, 0)):
-
+        """Instantiation"""
         if type(size) is not int:
             raise TypeError('size must be an integer')
         if size < 0:
