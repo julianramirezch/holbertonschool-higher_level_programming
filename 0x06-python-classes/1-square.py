13b8ee7d645512cfea31a569aae12da1f2eb51cd
julianramirezch <julianramirezch1@gmail.com>
2020-05-21 11:27:01 -0500

    [fix]: Documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-""" class Square that defines a square by: (based on 0-square.py)
-    Private instance attribute: size
-    Instantiation with size (no type/value verification)
-"""
+""" class Square that defines a square by: (based on 0-square.py) """
 
 
 class Square:
