83c2cd4a1798d639dff52704c9308a5296d1addc
julianramirezch <julianramirezch1@gmail.com>
2020-05-21 11:34:27 -0500

    [fix]: Documentation

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,13 +3,9 @@
 
 
 class Square:
-    """ Class define a Square"""
-
-    def __init__(self, size=0, area=0):
-        """
+    """ Class define a Square
             Args:
                 size (int): Size of Square.
-                area (int): Area of Square
 
             Returns:
                 None.
@@ -18,39 +14,18 @@ class Square:
                 TypeError: If size is not an integer
                 ValueError: If `size` is minor to 0.
         """
+    def __init__(self, size=0, area=0):
         self.size = size
         self.__area = area
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
 
     @property
     def size(self):
-        """
-            Args:
-                self : Square.
-
-            Return:
-                self.__size
-        """
+        """ Getter of size"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Set value of size
-
-            Args:
-                self : Square.
-                value (int): Value of size
-
-            Returns:
-                None.
-
-            Raises:
-                TypeError: If `value` is not an integer
-                ValueError: If `value` is minor to 0.
-        """
+        """ Set value of size """
         self.__size = value
         if type(value) is not int:
             raise TypeError('size must be an integer')
@@ -58,11 +33,5 @@ class Square:
             raise ValueError('size must be >= 0')
 
     def area(self):
-        """
-            Args:
-                self : Square.
-
-            Return:
-                Current square area.
-        """
+        """ Return current square area """
         return (self.__size ** 2)
