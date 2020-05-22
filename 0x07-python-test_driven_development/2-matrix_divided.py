92db347a99f79f6adf27a20e5ead99c541e7e310
julianramirezch <julianramirezch1@gmail.com>
2020-05-22 12:09:35 -0500

    [fix]: Test errors

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,27 +9,31 @@ def matrix_divided(matrix, div):
     row_error = 'Each row of the matrix must have the same size'
     div_error = 'div must be a number'
     div_list = []
-    size = len(matrix[0])
 
-    if type(matrix) is not list:
-        raise TypeError(matrix_error)
-    if type(div) not in [float, int]:
-        raise TypeError(div_error)
-    if div == 0:
-        raise ZeroDivisionError('division by zero')
+    if matrix == []:
+        raise IndexError(matrix_error)
+    else:
+        size = len(matrix[0])
 
-    for items in matrix:
-        if type(items) is not list:
+        if type(matrix) is not list:
             raise TypeError(matrix_error)
-        row = []
-        row_size = 0
-        for num in items:
-            if type(num) not in [float, int]:
-                raise TypeError(matrix_error)
-            row.append(round((num / div), 2))
-        row_size = len(row)
-        if size != row_size:
-            raise TypeError(row_error)
-        div_list.append(row)
+        if type(div) not in [float, int] or div is None:
+            raise TypeError(div_error)
+        if div == 0:
+            raise ZeroDivisionError('division by zero')
 
-    return div_list
+        for items in matrix:
+            if type(items) is not list:
+                raise TypeError(matrix_error)
+            row = []
+            row_size = 0
+            for num in items:
+                if type(num) not in [float, int]:
+                    raise TypeError(matrix_error)
+                row.append(round((num / div), 2))
+            row_size = len(row)
+            if size != row_size:
+                raise TypeError(row_error)
+            div_list.append(row)
+
+        return div_list
