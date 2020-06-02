83dd639e01725cd9a8277bed49938df024c3bdb4
julianramirezch <julianramirezch1@gmail.com>
2020-06-02 15:16:14 -0500

    [add]: prints the list, but sorted (ascending sort)

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module: 1-my_list -  inherits from list , prints sorted list"""
+
+
+class MyList(list):
+    """Class define MyList"""
+    def print_sorted(self):
+        print(sorted(self))
