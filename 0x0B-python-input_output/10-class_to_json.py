3458a82d874664a709f316ed445fcd4436295f86
julianramirezch <julianramirezch1@gmail.com>
2020-06-03 16:54:51 -0500

    [fix]: Delete impor json

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -2,9 +2,6 @@
 """ Module: 10-class_to_json """
 
 
-import json
-
-
 def class_to_json(obj):
     """function that returns the dictionary description with simple data
     structure (list, dictionary, string, integer and boolean) for JSON
