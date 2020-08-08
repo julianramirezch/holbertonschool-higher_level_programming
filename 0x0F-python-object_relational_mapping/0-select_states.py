6592abaea895a4b6a4b1e1c9ac4f87eb97800651
julianramirezch <julianramirezch1@gmail.com>
2020-08-08 00:42:55 -0500

    [refactor]: delete function connect_database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,6 +6,12 @@ from sys import argv
 
 def get_states(db):
     ''' Get states from hbtn_0e_0_usa database '''
+    db = connect_database('localhost', 3306, argv[1], argv[2], argv[3])
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     try:
         cur = db.cursor()
         cur.execute('SELECT * FROM states ORDER BY id ASC')
@@ -19,12 +25,5 @@ def get_states(db):
         print(state)
 
 
-def connect_database(host, port, user, passwd, db):
-    ''' Function connect database '''
-    db = MySQLdb.connect(host=host, port=port, user=user, passwd=passwd, db=db)
-    return(db)
-
-
 if __name__ == "__main__":
-    db = connect_database('localhost', 3306, argv[1], argv[2], argv[3])
     states = get_states(db)
