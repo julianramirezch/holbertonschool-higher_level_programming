cc45aa2e578b0ab28977319a5157dbe6a4180999
julianramirezch <julianramirezch1@gmail.com>
2020-08-08 05:55:22 +0000

    [refactor]

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,19 +6,22 @@ from sys import argv
 
 def get_states(db):
     ''' Get states from hbtn_0e_0_usa database '''
-    cur = db.cursor()
-    cur.execute('SELECT * FROM states ORDER BY id ASC')
-    states = cur.fetchall()
-
-    cur.close()
-    db.close()
-    return states
+    try:
+        cur = db.cursor()
+        cur.execute('SELECT * FROM states ORDER BY id ASC')
+        states = cur.fetchall()
+    except MySQLdb.Error as e:
+        print('MySQL Error {}: {}'.format(e.args[0], e.args[1]))
+    finally:
+        cur.close()
+        db.close()
+        return(states)
 
 
 def connect_database(host, port, user, passwd, db):
     ''' Function connect database '''
     db = MySQLdb.connect(host=host, port=port, user=user, passwd=passwd, db=db)
-    return db
+    return(db)
 
 
 if __name__ == "__main__":
