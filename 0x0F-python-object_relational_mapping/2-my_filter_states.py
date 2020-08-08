2ec0a951a275ac2a24a9f9888b9394e5fb62a43c
julianramirezch <julianramirezch1@gmail.com>
2020-08-08 17:07:42 +0000

    [fix]: SQL statement

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,8 @@ def get_data(db, name):
     ''' Get data from hbtn_0e_0_usa database '''
     try:
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name='{}'".format(name))
+        cur.execute("SELECT * FROM states WHERE name='{}'\
+                     ORDER BY id ASC".format(name))
         state = cur.fetchall()
     except MySQLdb.Error as e:
         print('MySQL Error {}: {}'.format(e.args[0], e.args[1]))
