6dbc0605ea03c131eca0b4a437bc9ef941f48cfa
julianramirezch <julianramirezch1@gmail.com>
2020-08-08 05:49:31 +0000

    [refactor]

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,13 +4,8 @@ import MySQLdb
 from sys import argv
 
 
-if __name__ == "__main__":
+def get_states(db):
     ''' Get states from hbtn_0e_0_usa database '''
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
     try:
         cur = db.cursor()
         cur.execute('SELECT * FROM states ORDER BY id ASC')
@@ -20,5 +15,17 @@ if __name__ == "__main__":
     finally:
         cur.close()
         db.close()
+        return(states)
+
+
+def connect_database(host, port, user, passwd, db):
+    ''' Function connect database '''
+    db = MySQLdb.connect(host=host, port=port, user=user, passwd=passwd, db=db)
+    return(db)
+
+
+if __name__ == "__main__":
+    db = connect_database('localhost', 3306, argv[1], argv[2], argv[3])
+    states = get_states(db)
     for state in states:
         print(state)
