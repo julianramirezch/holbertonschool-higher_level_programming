ccdf14516aa914a8f99331f80142eee3d63b8e2a
julianramirezch <julianramirezch1@gmail.com>
2020-08-08 03:43:06 +0000

    [add]: script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+
+def get_states(db):
+    ''' Get states from hbtn_0e_0_usa database '''
+    try:
+        cur = db.cursor()
+        cur.execute("""SELECT * FROM states ORDER BY id ASC""")
+        states = cur.fetchall()
+    except MySQLdb.Error as e:
+        print('MySQL Error {}: {}'.format(e.args[0], e.args[1]))
+    finally:
+        cur.close()
+        db.close()
+        return(states)
+
+
+def connect_database(host, port, user, passwd, db):
+    ''' Function conect database '''
+    db = MySQLdb.connect(host=host,
+                         port=port,
+                         user=user,
+                         passwd=passwd,
+                         db=db)
+    return(db)
+
+
+if __name__ == "__main__":
+    db = connect_database('localhost', 3306, argv[1], argv[2], argv[3])
+    states = get_states(db)
+    for state in states:
+        print(state)
