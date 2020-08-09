d29957019651c3d6f75e7346de81ffe9f58899e4
julianramirezch <julianramirezch1@gmail.com>
2020-08-09 18:56:53 +0000

    [add]: Write a script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Write a script that lists all State objects that contain the letter a from the database  database hbtn_0e_6_usa
+    using SQLAlchemy """
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def get_data(engine):
+    ''' Get data from database '''
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states_data = session.query(State).order_by(State.id).\
+        filter(State.name.like('%a%')).all()
+    session.close()
+
+    return (states_data)
+
+
+def connect_database(user, passwd, db):
+    ''' Connect to database '''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
+    return (engine)
+
+
+if __name__ == "__main__":
+    engine = connect_database(argv[1], argv[2], argv[3])
+    data = get_data(engine)
+    for state in data:
+        print('{}: {}'.format(state.id, state.name))
