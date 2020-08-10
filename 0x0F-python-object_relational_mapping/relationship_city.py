d9df6b338abdcc5b27117571046df7e4d259c435
julianramirezch <julianramirezch1@gmail.com>
2020-08-10 02:24:03 +0000

    [add]: Improve the files model_city.py and model_state.py, and save them as relationship_city.py and relationship_state.py

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""  class definition of a State and an instance Base = declarative_base() """
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from relationship_state import Base, State
+
+Base = declarative_base()
+
+
+class City(Base):
+    """ Class City """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
