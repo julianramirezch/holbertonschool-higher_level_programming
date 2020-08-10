64ece943f7757ccba04555e5223481bf6d25ab4e
julianramirezch <julianramirezch1@gmail.com>
2020-08-10 02:23:11 +0000

    [add]: script that creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa: (100-relationship_states_cities.py)

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" script that creates the State “California” with the City “San Francisco”
+from the database hbtn_0e_100_usa: (100-relationship_states_cities.py) using
+SQLAlchemy """
+
+from sys import argv
+from relationship_city import City
+from relationship_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def get_data(engine):
+    ''' Get data from database '''
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    row_state = State(name='California')
+    row_city = City(name='San Francisco')
+    row_state.cities = [row_city]
+    session.add(row_state)
+    session.commit()
+    session.close()
+
+
+def connect_database(user, passwd, db):
+    ''' Connect to database '''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
+    return (engine)
+
+
+if __name__ == "__main__":
+    engine = connect_database(argv[1], argv[2], argv[3])
+    get_data(engine)
