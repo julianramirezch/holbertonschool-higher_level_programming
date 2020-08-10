3a420839d8937e7381d832306c26468a376da08b
julianramirezch <julianramirezch1@gmail.com>
2020-08-10 02:46:28 +0000

    [delete]: Base = declarative_base()

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -3,9 +3,7 @@
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from relationship_state import Base, State
-
-Base = declarative_base()
+from relationship_state import Base
 
 
 class City(Base):
