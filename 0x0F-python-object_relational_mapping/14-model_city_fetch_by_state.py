b81d29831f9ea3b9ed49e6214fe01f89db072869
julianramirezch <julianramirezch1@gmail.com>
2020-08-10 01:52:35 +0000

    [add]: Python file similar to model_state.py named model_city.py that contains the class definition of a City and script Python file similar to model_state.py named model_city.py that contains the class definition of a City

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" script that lists all State objects from the database hbtn_0e_6_usa
+    using SQLAlchemy """
+
+from sys import argv
+from model_city import Base, City
+from model_state import State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def get_data(engine):
+    ''' Get data from database '''
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    data = session.query(City, State).filter(City.state_id == State.id).\
+        order_by(City.id).all()
+    session.close()
+
+    return (data)
+
+
+def connect_database(user, passwd, db):
+    ''' Connect to database '''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
+    return (engine)
+
+
+if __name__ == "__main__":
+    engine = connect_database(argv[1], argv[2], argv[3])
+    data = get_data(engine)
+    for city in data:
+        print('{}: ({}) {}'.format(city[1].name, city[1].id, city[0].name))
