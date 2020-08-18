7ec65b2c654c58943c289e5af76d1b3610c80d0c
julianramirezch <julianramirezch1@gmail.com>
2020-08-18 17:14:51 -0500

    [refactor]

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,18 +4,20 @@
 
 def find_peak(list_of_integers):
     """ function that finds a peak in a list of unsorted integers """
-    lenght = len(list_of_integers) - 1
-    if lenght <= 0:
-        return None
+    lenght = len(list_of_integers)
+    if lenght != 0:
+        idx = find_peak_2(list_of_integers, 0, lenght, lenght)
+        return list_of_integers[idx]
 
-    middle = int(lenght / 2)
-    right = list_of_integers[middle]
-    left = list_of_integers[middle - 1]
 
-    if (middle == lenght or right >= list_of_integers[middle + 1])\
-            and (middle == 0 or right >= left):
-        return right
-    elif middle != lenght and list_of_integers[middle + 1] > right:
-        idx = middle + 1
-        return find_peak(list_of_integers[idx:])
-    return find_peak(list_of_integers[:middle])
+def find_peak_2(list_n, low, high, lenght):
+    middle = int(low + (high - low)/2)
+    # print('middle {}'.format(middle))
+
+    if (middle == 0 or list_n[middle - 1] <= list_n[middle]) and\
+            (middle == lenght - 1 or list_n[middle + 1] <= list_n[middle]):
+        return middle
+    elif middle > 0 and list_n[middle - 1] > list_n[middle]:
+        return find_peak_2(list_n, low, (middle - 1), lenght)
+    else:
+        return find_peak_2(list_n, (middle + 1), high, lenght)
