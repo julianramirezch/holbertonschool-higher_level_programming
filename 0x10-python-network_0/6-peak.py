c922d7177e0a98be33a937bf06e5d8afb0681d77
julianramirezch <julianramirezch1@gmail.com>
2020-08-18 17:54:26 -0500

    [add]: return any of the peaks in the list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,23 +2,28 @@
 """  finds a peak in a list of unsorted integers """
 
 
+# def find_peak(list_of_integers):
+#     """ function that finds a peak in a list of unsorted integers """
+#     lenght = len(list_of_integers)
+#     if lenght != 0:
+#         idx = find_peak_2(list_of_integers, 0, lenght, lenght)
+#         # print('idx {}'.format(idx))
+#         return list_of_integers[idx]
+
+
+# def find_peak_2(list_n, low, high, lenght):
+#     middle = int(low + (high - low)/2)
+#     # print('middle {} lenght {}'.format(middle, lenght))
+
+#     if (middle == 0 or list_n[middle - 1] <= list_n[middle]) and\
+#             (middle == lenght - 1 or list_n[middle + 1] <= list_n[middle]):
+#         return middle
+#     if middle > 0 and list_n[middle - 1] > list_n[middle]:
+#         return find_peak_2(list_n, low, middle, lenght)
+#     else:
+#         return find_peak_2(list_n, middle, high, lenght)
+
 def find_peak(list_of_integers):
     """ function that finds a peak in a list of unsorted integers """
-    lenght = len(list_of_integers)
-    if lenght != 0:
-        idx = find_peak_2(list_of_integers, 0, lenght, lenght)
-        # print('idx {}'.format(idx))
-        return list_of_integers[idx]
-
-
-def find_peak_2(list_n, low, high, lenght):
-    middle = int(low + (high - low)/2)
-    # print('middle {} lenght {}'.format(middle, lenght))
-
-    if (middle == 0 or list_n[middle - 1] <= list_n[middle]) and\
-            (middle == lenght - 1 or list_n[middle + 1] <= list_n[middle]):
-        return middle
-    if middle > 0 and list_n[middle - 1] > list_n[middle]:
-        return find_peak_2(list_n, low, middle, lenght)
-    else:
-        return find_peak_2(list_n, middle, high, lenght)
+    if list_of_integers:
+        return (sorted(list_of_integers)[-1])
