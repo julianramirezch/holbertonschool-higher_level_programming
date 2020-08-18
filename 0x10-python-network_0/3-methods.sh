ea4c15ec67e972298aa5c0d3c8626bcdb5b996e4
julianramirezch <julianramirezch1@gmail.com>
2020-08-18 12:21:46 -0500

    [refactor]: change awk to cut

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -Is "$1" | grep "Allow" | awk '{print $2, $3, $4}'
+curl -Is "$1" | grep "Allow" | cut -d " " -f2
