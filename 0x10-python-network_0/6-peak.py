4e2e75a4176716605e901311f4437dfc6a9c4b82
julianramirezch <julianramirezch1@gmail.com>
2020-08-18 18:02:15 -0500

    [add]: return any of the peaks in the list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -26,6 +26,5 @@
 def find_peak(list_of_integers):
     """ function that finds a peak in a list of unsorted integers """
     if len(list_of_integers) != 0:
-        return (sorted(list_of_integers)[-1])
-    else:
-        return None
+        list_of_integers.sort()
+        return (list_of_integers[-1])
