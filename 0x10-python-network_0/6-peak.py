76f6f87be1c918afd94a97dcf035b2b7bb7e529a
julianramirezch <julianramirezch1@gmail.com>
2020-08-18 17:28:17 -0500

    [delete]: Delete middle conditions

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,17 +7,18 @@ def find_peak(list_of_integers):
     lenght = len(list_of_integers)
     if lenght != 0:
         idx = find_peak_2(list_of_integers, 0, lenght, lenght)
+        # print('idx {}'.format(idx))
         return list_of_integers[idx]
 
 
 def find_peak_2(list_n, low, high, lenght):
     middle = int(low + (high - low)/2)
-    # print('middle {}'.format(middle))
+    # print('middle {} lenght {}'.format(middle, lenght))
 
-    if (middle == 0 or list_n[middle - 1] <= list_n[middle]) and\
-            (middle == lenght - 1 or list_n[middle + 1] <= list_n[middle]):
+    if (list_n[middle - 1] <= list_n[middle]) and\
+            (list_n[middle + 1] <= list_n[middle]):
         return middle
-    elif middle > 0 and list_n[middle - 1] > list_n[middle]:
+    if middle > 0 and list_n[middle - 1] > list_n[middle]:
         return find_peak_2(list_n, low, middle, lenght)
     else:
         return find_peak_2(list_n, middle, high, lenght)
