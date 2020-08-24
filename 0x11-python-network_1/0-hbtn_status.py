c8a525c3a87b833e99871bdb10545e4fc6e88efb
julianramirezch <julianramirezch1@gmail.com>
2020-08-24 12:15:42 -0500

    [fix]: TYPE

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 ''' Python script that fetches https://intranet.hbtn.io/status '''
-import urllib.request
+import urllib.request as ur
 
 
 def fetch_url(url):
     ''' Function fetch url '''
-    with urllib.request.urlopen(url) as response:
+    with ur.urlopen(url) as response:
         html = response.read()
 
     return html
@@ -14,6 +14,6 @@ def fetch_url(url):
 if __name__ == "__main__":
     html = fetch_url('https://intranet.hbtn.io/status')
     print('Body response:')
-    print('    - type: {}'.format(html))
+    print('    - type: {}'.format(type(html)))
     print('    - content: {}'.format(html))
     print('    - utf8 content: {}'.format(html.decode('utf-8')))
