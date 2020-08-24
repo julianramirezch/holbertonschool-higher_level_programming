cb68a30935a6ab51b0c103e1d843f3e7c8208ffd
julianramirezch <julianramirezch1@gmail.com>
2020-08-24 09:32:53 -0500

    [refactor]: add main function

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,18 @@
 ''' Python script that fetches https://intranet.hbtn.io/status '''
 import urllib.request as ur
 
-url = 'https://intranet.hbtn.io/status'
-with ur.urlopen(url) as response:
-    html = response.read()
+
+def fetch_url(url):
+    ''' Function fetch url '''
+    with ur.urlopen(url) as response:
+        html = response.read()
+
+    return html
+
+
+if __name__ == "__main__":
+    html = fetch_url('https://intranet.hbtn.io/status')
     print('Body response:')
-    print('    - type: {}'.format(type(response.read())))
+    print('    - type: {}'.format(html))
     print('    - content: {}'.format(html))
     print('    - utf8 content: {}'.format(html.decode('utf-8')))
