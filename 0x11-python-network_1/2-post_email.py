fc62b96f969dfbc0eba770a104f31cf595e5700e
julianramirezch <julianramirezch1@gmail.com>
2020-08-24 10:11:18 -0500

    [add]: Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''  Python script that takes in a URL and an email, sends a POST request to
+the passed URL with the email as a parameter, and displays the body of the
+response (decoded in utf-8)'''
+
+import urllib.request
+import urllib.parse
+from sys import argv
+
+
+def fetch_url(url, email):
+    ''' Fetch url '''
+    values['email'] = email
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+
+    return html
+
+
+if __name__ == "__main__":
+    html = fetch_url(argv[1], argv[2])
+    print(html)
