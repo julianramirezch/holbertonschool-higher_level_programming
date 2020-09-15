2656ba5e382a23cabef42f5b7e82824aa9acdc80
julianramirezch <julianramirezch1@gmail.com>
2020-09-15 11:58:24 -0500

    [add]: script that prints the addition of 2 integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const a = parseInt(process.argv[2], 10);
+const b = parseInt(process.argv[3], 10);
+
+function add (a, b) {
+  return a + b;
+}
+
+if (!isNaN(a) && !isNaN(b)) {
+  console.log(add(a, b));
+} else {
+  console.log(a + b);
+}
