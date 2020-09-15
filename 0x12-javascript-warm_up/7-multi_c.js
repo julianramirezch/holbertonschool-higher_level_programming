6b72023876d7acecb1aa4cbdfc12adf9757cbd37
julianramirezch <julianramirezch1@gmail.com>
2020-09-15 11:23:00 -0500

    [add]: script that prints x times “C is fun”

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2], 10);
+
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
+}
+
+for (let i = 0; i < num; i++) {
+  console.log('C is fun');
+}
