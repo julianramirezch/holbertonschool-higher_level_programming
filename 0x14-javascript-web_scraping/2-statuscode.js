f159be9c299509d3069d2889fef6387ddedf3013
julianramirezch <julianramirezch1@gmail.com>
2020-09-22 13:45:44 -0500

    [fix]: Handle error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,6 @@
 const url = process.argv[2];
 const request = require('request');
 request(url, (error, response, body) => {
+  if (error) { console.log(error); }
   console.log('code: ' + response.statusCode);
 });
